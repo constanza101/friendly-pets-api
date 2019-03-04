95a59c5f473d715275f87973567a77779045d6c6
Constanza Díaz <constanza.diaz@gmail.com>
2019-03-04 16:52:49 +0100

    post city address user

---
+++ method-path-server.js
@@ -0,0 +1,68 @@
+"This document is a guide/index of api/server.js"
+*************************************************************
+"READ LIKE:"
+METHOD/path -> (posting order) -- response type
+(if value = 1 -> value_type: number)
+*************************************************************
+>> USER
+//signup:
+POST/city -> (1) -- {"id": 1,"name": "Barcelona"}
+POST/address -> (2) -- {"id":address_id, "address": new_address,"postal_code": new_PC, "city_id": new_cityID }
+POST/user -> (3) -- [{ "id": 26, "name": "Rosario", "email": "rosario@gmail.com", "password": "7110eda4d09e062aa5e4a390b0a572ac0d2c0220", "address_id": 1, "creation_date": "2019-03-04T14:43:10.000Z"}]
+
+//USER profile:
+GET/user/:id -> [{ "id": 1, "name": user_name, "email": user_email, "password": user_pass, "address_id": 1, "creation_date": "2019-02-26T15:48:48.000Z"}]
+//USER update profile:
+UPDATE/user/:id -> nofunciona
+      -> debe recibir el tipo:
+      /*[{"key":"email", "value":"mauritoelchacarerito@hotmail.com"},
+      {"key":"name", "value":"hgjhg"},
+      {"key":"address_id", "value":1}]*/
+------------------------------------------------------------------------------
+>> ANIMAL
+//NANIMAL register new.
+owner_user_id >> id
+id,
+name,
+animal_type_id, >> id
+//animal_breed_id >> id
+birthdate,
+gender,
+size,
+picture,
+description,
+creation_date
+status = default
+//status changes --> updates
+>> ADOPTION :
+new_status = adoption
+care = quiet/energetic/special care
+>> LOST :
+new_status = lost
+lost_address_id >> id
+lost_date
+>> FOUND :
+new_status = found
+found_address_id >> id
+found_date
+
+POST/animal --> {"owner_user_id": 1, "name": "Calu", "animal_type_id": 1, "birthdate": null, "gender": "F", "size": "large", "picture": "www.google.com", "description": "es una gatita bebe"}
+GET/animals/:owner_user_id --> (returns all the pets of one user) -> [{},{}]
+[
+{ "id": 2, "name": "Alfredo", "animal_type_id": 2, "birthdate": "2015-02-01T23:00:00.000Z", "gender": "M", "size": "small", "picture": null, "status": "default", "description": "Alfredo esta loquito de amor", "creation_date": "2019-02-26T15:56:03.000Z", "lost_date": null, "lost_address_id": null, "found_date": null, "found_address_id": null, "owner_user_id": 2, "animal_breed_id": 2},
+{ "id": 21, "name": "Calu", "animal_type_id": 1, "birthdate": null, "gender": "F", "size": "large", "picture": "www.google.com", "status": "default", "description": "es una gatita bebe", "creation_date": "2019-03-03T16:55:49.000Z", "lost_date": null, "lost_address_id": null, "found_date": null, "found_address_id": null, "owner_user_id": 2, "animal_breed_id": null}
+]
+UPDATE/animal_lost/:id
+
+
+
+
+
+
+
+
+
+
+
+
+.
---
+++ server.js
@@ -0,0 +1,189 @@
+var fs = require ("fs");
+var colors = require ("colors");
+var express = require("express");
+var mysql = require("mysql");
+var connectionData  = process.argv[2]; /*eslint no-undef: "off"*/
+
+fs.readFile( connectionData+".json", function (err, data) {
+  if (err) throw err;
+  var objServerData = JSON.parse(data);
+
+  var connection = mysql.createConnection(objServerData);
+
+  //abro la conexión
+  connection.connect();
+  console.log("conectado!");
+
+  //creo mi servidor:
+  var app = express();
+
+  //configuro el server para que parsee el body de las peticiones post
+  app.use(express.json());
+  app.use(express.urlencoded());
+
+
+  //POST/city
+  app.post("/city", function(req, res){
+    var new_city = req.body.name;
+    //condición para saber si la nueva ciudad ya existe.
+    connection.query("SELECT * FROM city", function (err, data) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].name == req.body.name) {
+          console.log("ya existe esta ciudad en la base de datos".green);
+          return res.send(data[i]) ; // devuelve un JSON con la ciudad y el id, y lo puedo usar para crear el address.
+        }
+      }
+    //query para agregar una nueva ciudad
+    connection.query( "INSERT INTO city (name) VALUES('"+new_city+"');",function (err, data) {
+          return res.send({"id":data.insertId, "city": new_city});
+            });
+      });
+    });
+
+
+  //POST/address
+  app.post("/address", function(req, res){
+    var new_address = req.body.address;
+    var new_PC = req.body.postal_code;
+    var new_cityID = req.body.city_id;
+    connection.query(
+      "INSERT INTO address (address, postal_code, city_id) VALUES("
+          +"'"+new_address+"','"+new_PC+"',"+new_cityID+");"
+      ,function (err, data) {
+          return res.send({"id":data.insertId, "address": new_address,"postal_code": new_PC, "city_id": new_cityID});
+        });
+    });
+
+  //POST/user
+  app.post("/user", function(req, res){
+    var new_name = req.body.name;
+    var new_email = req.body.email;
+    var new_password = req.body.password;
+    var new_addressID = req.body.address_id;
+    connection.query("INSERT INTO user (name, email, password, address_id) VALUES("
+              +"'"+new_name+"','"+new_email+"',"+"SHA1("+"'"+new_password+"'"+")"+","+new_addressID+");"
+          ,function (err, data) {
+            var new_userID = data.insertId
+            console.log(data);
+            connection.query("SELECT * FROM user WHERE id =("+new_userID+");"
+                  ,function (err, data) {
+                    console.log(new_userID);
+                  return res.send(data);
+                    });
+    });
+  });
+
+
+    //GET/user/:id - GET USER INFO FOR PROFILE
+    app.get("/user/:id", function(req, res){
+      var id = req.params.id;
+
+      connection.query("SELECT * FROM user WHERE id =("+id+");"
+        ,function (err, data) {
+            return res.send(data);
+          });
+      });
+
+
+
+
+/*
+      //UPDATE/user/:id - UPDATE USER INFO - nofunciona - el bucle funciona pero los valores no van.
+      app.put("/user/:id", function(req, res){
+        var id = req.params.id;
+        var new_value = req.body
+
+        for(var i= 0, l = new_value.length; i< l; i++){
+          console.log(new_value[i]);
+          console.log(new_value[i].key);
+
+          connection.query("UPDATE user SET "+new_value.key+"="+new_value.value+" WHERE id =("+id+");"
+            ,function (err, data) {
+              console.log(new_value[i].key)
+              return res.send(new_value[i].key);
+          })   //UPDATE
+        }  //for
+      }); //app.put
+*/
+
+
+
+//POST/animal  nofunciona: birthdate.por ahora poniendo null puedo cargar todo lo demas pero no puedo dar con el formato de la fecha.
+
+//POST  - owner_user_id id, name, animal_type_id, birthdate, gender, size, picture, description, creation_date, status
+//update for adoption: just change: status = adoption
+//update to lost: status=lost, lost_date, lost_address_id,
+//ipdate to found: status=found, found_date, found_address_id, owner_user_id, animal_breed_id
+
+app.post("/animal", function(req, res){
+  var new_animal_id = data.insertId // se agrega solo al registrar el animal
+  var owner_user_id = req.body.owner_user_id;
+  var name = req.body.name;
+  var animal_type_id = req.body.animal_type_id;
+  var animal_breed_id = req.body.animal_breed_id;
+  var birthdate = req.body.birthdate;
+  var gender = req.body.gender;
+  var size = req.body.size;
+  var picture = req.body.picture;
+  var description = req.body.description;
+  var creation_date = req.body.creation_date; //se agrega sola al registrar el animal
+  var status = req.body.status;
+  //update to lost
+  var lost_date = req.body.lost_date;
+  var lost_address_id = req.body.lost_address_id;
+  //update to found
+  var found_date = req.body.found_date;
+  var found_address_id = req.body.found_address_id;
+/*
+  console.log("INSERT INTO animal (owner_user_id, name, animal_type_id, birthdate, gender, size, picture, description) VALUES("
+              +owner_user_id+",'"+name+"',"+animal_type_id+","+birthdate+",'"+gender+"','"+size+"','"+picture+"','"+description+"');");
+*/
+
+  connection.query("INSERT INTO animal (owner_user_id, name, animal_type_id, birthdate, gender, size, picture, description) VALUES("
+              +owner_user_id+",'"+name+"',"+animal_type_id+","+birthdate+",'"+gender+"','"+size+"','"+picture+"','"+description+"');"
+        ,function (err, data) {
+        //return res.send(JSON.stringify(data));
+        return res.send(req.body);
+          });
+
+
+});
+
+
+//GET/user/:id - GET USER INFO FOR PROFILE
+app.get("/animals/:owner_user_id", function(req, res){
+  var owner_user_id = req.params.owner_user_id;
+
+  connection.query("SELECT * FROM animal WHERE owner_user_id =("+owner_user_id+");"
+        ,function (err, data) {
+          //console.log(data);
+          return res.send(data)
+          });
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    app.listen(8000, function(){
+      console.log("Server is listening in port 8000")
+    })
+
+});
