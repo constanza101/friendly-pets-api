f874a82b26e424e543175dc84ecf64af30317ad2
Constanza Díaz <constanza.diaz@gmail.com>
2019-03-05 18:08:35 +0100

    get * animals of same owner

--- method-path-server.js
+++ method-path-server.js
@@ -15,13 +15,13 @@ POST/user -> (3) -- [{ "id": 26, "name": "Rosario", "email": "rosario@gmail.com"
 GET/user/:id -> [{ "id": 1, "name": user_name, "email": user_email, "password": user_pass, "address_id": 1, "creation_date": "2019-02-26T15:48:48.000Z"}]
 //USER update profile:
 UPDATE/user/:id
-      -> req.body:
+      -> receives_req.body:
       [
         {"key":"email", "value":"mauritoelchacarerito@hotmail.com"},
         {"key":"name", "value":"Mauro"},
         {"key":"address_id", "value":1}
       ]
-      -> res.send():
+      -> response_res.send():
       [{
         "id": 10,
         "name": "Betito",
@@ -31,7 +31,7 @@ UPDATE/user/:id
         "creation_date": "2019-03-04T13:38:41.000Z"
       }]
 
-DELETE/user/:id -> res.send(data) :
+DELETE/user/:id -> response_res.send(data) :
                             {
                             "fieldCount": 0,
                             "affectedRows": 1,
@@ -72,14 +72,39 @@ new_status = found
 found_address_id >> id
 found_date
 
-POST/animal --> {"owner_user_id": 1, "name": "Calu", "animal_type_id": 1, "birthdate": null, "gender": "F", "size": "large", "picture": "www.google.com", "description": "es una gatita bebe"}
+
+POST/animal
+    --> req.body
+      {
+        "owner_user_id": 1,
+        "name": "Calu",
+        "animal_type_id": 1, //>>>> 1 = perro; 2 = gato
+        "birthdate": null,
+        "gender": "F",// F = female; M = male
+        "size": "large",
+        "picture": "url",
+        "description": "es una gatita bebe"
+        "id": 1
+      }
+
+
 GET/animals/:owner_user_id --> (returns all the pets of one user) -> [{},{}]
 [
 { "id": 2, "name": "Alfredo", "animal_type_id": 2, "birthdate": "2015-02-01T23:00:00.000Z", "gender": "M", "size": "small", "picture": null, "status": "default", "description": "Alfredo esta loquito de amor", "creation_date": "2019-02-26T15:56:03.000Z", "lost_date": null, "lost_address_id": null, "found_date": null, "found_address_id": null, "owner_user_id": 2, "animal_breed_id": 2},
 { "id": 21, "name": "Calu", "animal_type_id": 1, "birthdate": null, "gender": "F", "size": "large", "picture": "www.google.com", "status": "default", "description": "es una gatita bebe", "creation_date": "2019-03-03T16:55:49.000Z", "lost_date": null, "lost_address_id": null, "found_date": null, "found_address_id": null, "owner_user_id": 2, "animal_breed_id": null}
 ]
-UPDATE/animal_lost/:id
 
+
+TODO: continuar haciendo el update para animal. Poner un IF, para que cuando key sea "lost_date o found_date", que value sea new Date().:
+UPDATE/animal/:id
+        FOR ANIMAL LOST:
+              POST/city -> (1) -- {"id": 1,"name": "Barcelona"}
+              POST/address -> (2) -- {"id":address_id, "address": new_address,"postal_code": new_PC, "city_id": new_cityID }
+              -> (3)receives_req.body:
+              [
+                {"key":"lost_date", "value": date},
+                {"key":"lost_address_id", "value":1},
+              ]
 
 
 
--- server.js
+++ server.js
@@ -29,12 +29,14 @@ fs.readFile( connectionData+".json", function (err, data) {
     connection.query("SELECT * FROM city", function (err, data) {
       for (var i = 0; i < data.length; i++) {
         if (data[i].name == req.body.name) {
+          if(err) throw err;
           console.log("ya existe esta ciudad en la base de datos".green);
           return res.send(data[i]) ; // devuelve un JSON con la ciudad y el id, y lo puedo usar para crear el address.
         }
       }
     //query para agregar una nueva ciudad
     connection.query( "INSERT INTO city (name) VALUES('"+new_city+"');",function (err, data) {
+          if(err) throw err;
           return res.send({"id":data.insertId, "city": new_city});
             });
       });
@@ -50,6 +52,7 @@ fs.readFile( connectionData+".json", function (err, data) {
       "INSERT INTO address (address, postal_code, city_id) VALUES("
           +"'"+new_address+"','"+new_PC+"',"+new_cityID+");"
       ,function (err, data) {
+          if(err) throw err;
           return res.send({"id":data.insertId, "address": new_address,"postal_code": new_PC, "city_id": new_cityID});
         });
     });
@@ -67,8 +70,8 @@ fs.readFile( connectionData+".json", function (err, data) {
             console.log(data);
             connection.query("SELECT * FROM user WHERE id =("+new_userID+");"
                   ,function (err, data) {
-                    console.log(new_userID);
-                  return res.send(data);
+                    if(err) throw err;
+                    return res.send(data);
                     });
     });
   });
@@ -80,20 +83,22 @@ fs.readFile( connectionData+".json", function (err, data) {
 
       connection.query("SELECT * FROM user WHERE id =("+id+");"
         ,function (err, data) {
-            return res.send(data);
+          if(err) throw err;
+          return res.send(data);
           });
       });
 
 
-      //UPDATE/user/:id - UPDATE USER INFO - nofunciona - el bucle funciona pero los valores no van.
+      //UPDATE/user/:id
       app.put("/user/:id", function(req, res){
         var id = req.params.id;
         var new_value = req.body
 
         for(var i= 0; i<new_value.length; i++){
           //console.log(new_value[i].key);
-          connection.query("UPDATE user SET "+new_value[i].key+" = '"+new_value[i].value+"' WHERE id =(10);"
+          connection.query("UPDATE user SET "+new_value[i].key+" = '"+new_value[i].value+"' WHERE id =("+id+");"
             ,function (err, data) {
+              if(err) throw err;
               //console.log(new_value[i].key.green);
           })   //UPDATE
         }  //for
@@ -110,6 +115,7 @@ fs.readFile( connectionData+".json", function (err, data) {
         var id = req.params.id;
         connection.query("DELETE FROM user WHERE id =("+id+");"
           ,function (err, data) {
+            if(err) throw err;
               return res.send(data);
             });
         });
@@ -120,7 +126,7 @@ fs.readFile( connectionData+".json", function (err, data) {
 //POST  - owner_user_id id, name, animal_type_id, birthdate, gender, size, picture, description, creation_date, status
 //update for adoption: just change: status = adoption
 //update to lost: status=lost, lost_date, lost_address_id,
-//ipdate to found: status=found, found_date, found_address_id, owner_user_id, animal_breed_id
+//update to found: status=found, found_date, found_address_id, owner_user_id, animal_breed_id
 
 app.post("/animal", function(req, res){
   var new_animal_id = data.insertId // se agrega solo al registrar el animal
@@ -128,47 +134,72 @@ app.post("/animal", function(req, res){
   var name = req.body.name;
   var animal_type_id = req.body.animal_type_id;
   var animal_breed_id = req.body.animal_breed_id;
-  var birthdate = req.body.birthdate;
+  var birthdate = new Date(req.body.birthdate);
   var gender = req.body.gender;
   var size = req.body.size;
   var picture = req.body.picture;
   var description = req.body.description;
-  var creation_date = req.body.creation_date; //se agrega sola al registrar el animal
+  var creation_date = new Date(req.body.creation_date); //se agrega sola al registrar el animal
   var status = req.body.status;
   //update to lost
-  var lost_date = req.body.lost_date;
+  var lost_date = new Date(req.body.lost_date);
   var lost_address_id = req.body.lost_address_id;
   //update to found
-  var found_date = req.body.found_date;
+  var found_date = new Date(req.body.found_date);
   var found_address_id = req.body.found_address_id;
+
 /*
   console.log("INSERT INTO animal (owner_user_id, name, animal_type_id, birthdate, gender, size, picture, description) VALUES("
               +owner_user_id+",'"+name+"',"+animal_type_id+","+birthdate+",'"+gender+"','"+size+"','"+picture+"','"+description+"');");
 */
-
   connection.query("INSERT INTO animal (owner_user_id, name, animal_type_id, birthdate, gender, size, picture, description) VALUES("
-              +owner_user_id+",'"+name+"',"+animal_type_id+","+birthdate+",'"+gender+"','"+size+"','"+picture+"','"+description+"');"
-        ,function (err, data) {
-        //return res.send(JSON.stringify(data));
-        return res.send(req.body);
+              +owner_user_id+",'"+name+"',"+animal_type_id+","+connection.escape(birthdate)+",'"+gender+"','"+size+"','"+picture+"','"+description+"');"
+        ,function (err, result) {
+          if (err) throw err;
+        console.log(result.insertId);
+        var new_record = req.body;
+        new_record['id'] = result.insertId;
+        return res.send(new_record);
           });
 
 
 });
 
 
-//GET/user/:id - GET USER INFO FOR PROFILE
+//GET//animals/:owner_user_id - GET owner_user_id from animal.
 app.get("/animals/:owner_user_id", function(req, res){
   var owner_user_id = req.params.owner_user_id;
 
   connection.query("SELECT * FROM animal WHERE owner_user_id =("+owner_user_id+");"
         ,function (err, data) {
+          if(err) throw err;
+
           //console.log(data);
           return res.send(data)
           });
 
   });
 
+  //UPDATE/animal/:id
+  app.put("/animal/:id", function(req, res){
+    var id = req.params.id;
+    var new_value = req.body
+
+    for(var i= 0; i<new_value.length; i++){
+      //console.log(new_value[i].key);
+      connection.query("UPDATE animal SET "+new_value[i].key+" = '"+new_value[i].value+"' WHERE id =("+id+");"
+        ,function (err, data) {
+          if(err) throw err;
+          //console.log(new_value[i].key.green);
+      })   //UPDATE
+    }  //for
+
+    connection.query("SELECT * FROM user WHERE id =("+id+");"
+      ,function (err, data) {
+        if(err) throw err;
+          return res.send(data);
+        });//query select user by id
+  }); //app.put
 
 
 
